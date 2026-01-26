b5acc1990adedd27806a873ea8ba556c70b79056
gretaramadani <gretaramadani2008@gmail.com>
2026-01-26 20:26:18 +0100

    about page

---
+++ page_about.php
@@ -0,0 +1,56 @@
+<?php
+
+/* 
+
+Template Name: About Page
+
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="content" class="site-content">
+    <main id="main" class="site-main">
+        <section class="about-hero">
+            <div class="container">
+                <h1>About Us</h1>
+                <p>Welcome to our tech corner</p>
+            </div>
+        </section>
+        <section class="about-intro">
+            <div class="container">
+                <h2>Who we are</h2>
+                <p>We are team of developers</p>
+            </div>
+        </section>
+
+        <section class="about-mission">
+            <div class="container">
+                <h2>our mission</h2>
+                <p>our goal is simple....</p>
+            </div>
+        </section>
+
+        <section class="about-what-we-cover">
+            <div class="container">
+                <h2>What we cover</h2>
+                <ul>
+                    <li>Programming tutorials and tips</li>
+                    <li>Software and app reviews</li>
+                    <li>Web development, Devops and cloud computing</li>
+                    <li>Trends in tech and industry news.</li>
+                </ul>
+            </div>
+        </section>
+
+        <section class="about-contact">
+            <div class="container">
+                <h2>What to connect</h2>
+                <p>We love hearing from our readers!</p>
+            </div>
+        </section>
+    </main>
+        
+</div>
+
+<?php get_footer(); ?>
